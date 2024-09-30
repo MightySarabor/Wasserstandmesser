9839c678b5f6d0c0636b70a8e6f852b3f08fd0b4
MightySarabor <babaruschi@gmail.com>
2024-09-30 18:08:13 +0200

    final

--- Sender_Sensor/src/main.cpp
+++ Sender_Sensor/src/main.cpp
@@ -36,7 +36,7 @@ float getDistance() {
   digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
+  delayMicroseconds(1000);
   digitalWrite(TRIG_PIN, LOW);
   long duration = pulseIn(ECHO_PIN, HIGH);
   return (duration * 0.034) / 2;
